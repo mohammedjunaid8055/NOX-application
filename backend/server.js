64d90e6d2410669678a0cd476d68a4f9a889e60d
Mohammed Junaid <j27557343@gmail.com>
2026-03-31 02:53:46 +0530

    fix: express 5.x routing path error

--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,7 @@ app.use("/api/confessions", confessionRoutes);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/build")));
-  app.get("*", (req, res) => {
+  app.use((req, res) => {
     res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
   });
 } else {
